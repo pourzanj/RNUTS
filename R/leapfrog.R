d1faf124301c6b97139179fe66671a80101fcd06
Arya Pourzanjani <Arya@dhcp-46-204.cs.ucsb.edu>
2018-11-12 16:55:04 -0800

    leapfrog passing tests

--- R/leapfrog.R
+++ R/leapfrog.R
@@ -1,14 +1,16 @@
 take_one_step_lf <- function(z0, z_1, direction, ham_system) {
 
-  # unpack z0
+  # unpack z0 and Hamiltonian system
   q0 <- z0$q
   p0 <- z0$p
   h <- z0$h*direction
+
   GradU <- function(q) ham_system$compute_gradU(q)
+  M <- ham_system$M
 
   # leapfrog step
   p_half <- p0 - (h/2)*GradU(q0)
-  q1 <- q0 + h*(1/M)*p_half
+  q1 <- q0 + h*solve(M, p_half)
   p1 <- p_half - (h/2)*GradU(q1)
 
   # pack back in to z1
@@ -18,7 +20,7 @@ take_one_step_lf <- function(z0, z_1, direction, ham_system) {
   H0 <- ham_system$compute_H(z0)
   H1 <- ham_system$compute_H(z1)
   error <- NA
-  if (H0-H1 >= 1000) {
+  if (H1-H0 >= 1000) {
     error <- "Divergence"
     warning("Divergence detected")
   }
--- tests/testthat/test_leapfrog.R
+++ tests/testthat/test_leapfrog.R
@@ -1,10 +1,46 @@
 context("Leapfrog")
 
 test_that("Onestep Leapfrog Steps Correctly on 2D Gaussian", {
-})
+  M <- diag(c(1.0, 1.0))
+  Sigma <- matrix(c(1.0, 0.9, 0.9, 1.0), nrow = 2)
+  gaussian_system <- create_gaussian_hamiltonian_system(M, Sigma)
 
-test_that("Onestep Leapfrog Steps Correctly on 2D Funnel", {
+  # compute Leapfrog step manually
+  q0 <- c(0.0, 0.0)
+  p0 <- c(-0.1759228, 0.8358274)
+  p_half <- p0 - (0.1/2)*gaussian_system$compute_gradU(q0)
+  q1 <- q0 + 0.1*solve(M, p_half)
+  p1 <- p_half - (0.1/2)*gaussian_system$compute_gradU(q1)
+
+  # compute using library function
+  z0 <- list(q = c(0.0, 0.0), p = c(-0.1759228, 0.8358274), h = 0.1)
+  result <- take_one_step_lf(z0, z_1 = NULL, direction = 1, gaussian_system)
+  z1 <- result$z1
+
+  expect_equal(z1$q, q1)
+  expect_equal(z1$p, p1)
+  expect_equal(z1$h, 0.1)
 })
 
 test_that("Onestep Leapfrog Catches Divergences", {
+  M <- diag(c(1.0, 1.0))
+  Sigma <- matrix(c(1.0, 0.9, 0.9, 1.0), nrow = 2)
+  gaussian_system <- create_gaussian_hamiltonian_system(M, Sigma)
+
+  z0 <- list(q = c(0.0, 0.0), p = c(-0.1759228, 0.8358274), h = 0.1)
+  result <- take_one_step_lf(z0, z_1 = NULL, direction = 1, gaussian_system)
+  z1 <- result$z1
+
+  # try with a big step that should be divergent
+  z0 <- list(q = c(0.0, 0.0), p = c(-0.1759228, 0.8358274), h = 10.0)
+  result_divergent_1 <- take_one_step_lf(z0, z_1 = NULL, direction = 1, gaussian_system)
+  z1_divergent <- result_divergent_1$z1
+  result_divergent_2 <- take_one_step_lf(z1_divergent, z_1 = NULL, direction = 1, gaussian_system)
+  z2_divergent <- result_divergent_2$z1
+  result_divergent_3 <- take_one_step_lf(z2_divergent, z_1 = NULL, direction = 1, gaussian_system)
+  z3_divergent <- result_divergent_3$z1
+
+  expect_true(is.na(result$error))
+  print(result_divergent_3$error)
+  expect_identical(result_divergent_3$error, "Divergence")
 })
