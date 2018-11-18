b4c5d8330ee0c598e87e1843eb89c2e357a1f5d1
Arya Pourzanjani <Arya@dhcp-46-204.cs.ucsb.edu>
2018-11-18 11:55:09 -0800

    updated hamiltonian_systems.R to include a function for computing Hessians in case sometimes we want to do full solves instead Newton-Krylov solves

--- R/hamiltonian_systems.R
+++ R/hamiltonian_systems.R
@@ -2,6 +2,7 @@ create_gaussian_hamiltonian_system <- function(M, Sigma) {
 
   compute_U <- function(x) 0.5*sum(x*solve(Sigma,x))
   compute_gradU <- function(x) solve(Sigma,x)
+  compute_hessU <- function(x) solve(Sigma)
   compute_hessU_vec_prod <- function(q, v) solve(Sigma, v)
   D <- nrow(M)
   L <- chol(M)
@@ -9,6 +10,7 @@ create_gaussian_hamiltonian_system <- function(M, Sigma) {
   list(M = M,
        compute_H = function(z) 0.5*sum(z$p*solve(M,z$p)) + compute_U(z$q),
        compute_gradU = compute_gradU,
+       compute_hessU = compute_hessU,
        compute_hessU_vec_prod = compute_hessU_vec_prod,
        get_momentum_sample = function() L %*% rnorm(D))
 
@@ -26,6 +28,19 @@ create_funnel_hamiltonian_system <- function(M, D) {
     y <- q[1]
     x <- q[2:length(q)]
     c(y/9 + 0.5 - (x^2)/(2*exp(y)), x*exp(-y))
+  }
+
+  compute_hessU <- function(q) {
+    y <- q[1]
+    x <- q[2:length(q)]
+
+    # arrow matrix
+    H <- diag(length(q))
+    diag(H) <- c(1/9 + (x^2)/(2*exp(y)), rep(exp(-y), length(q)-1))
+    H[2:length(q), 1] <- -x*exp(-y)
+    H[1,2:length(q)] <- -x*exp(-y)
+
+    H
   }
 
   compute_hessU_vec_prod <- function(q, v) {
@@ -47,6 +62,7 @@ create_funnel_hamiltonian_system <- function(M, D) {
   list(M = M,
        compute_H = function(z) 0.5*sum(z$p*solve(M,z$p)) + compute_U(z$q),
        compute_gradU = compute_gradU,
+       compute_hessU = compute_hessU,
        compute_hessU_vec_prod = compute_hessU_vec_prod,
        get_momentum_sample = function() L %*% rnorm(D))
 }
@@ -62,7 +78,7 @@ create_funnel_hamiltonian_system <- function(M, D) {
 #' @export
 #'
 #' @examples
-create_custom_hamiltonian_system <- function(M, compute_U, compute_gradU, compute_hessU_vec_prod) {
+create_custom_hamiltonian_system <- function(M, compute_U, compute_gradU, compute_hessU, compute_hessU_vec_prod) {
 
   D <- length(M)
   L <- chol(M)
@@ -70,6 +86,7 @@ create_custom_hamiltonian_system <- function(M, compute_U, compute_gradU, comput
   list(M = M,
        compute_H = function(z) 0.5*sum(z$p*solve(M,z$p)) + compute_U(z$q),
        compute_gradU = compute_gradU,
+       compute_hessU = compute_hessU,
        compute_hessU_vec_prod = compute_hessU_vec_prod,
        get_momentum_sample = function() L %*% rnorm(D))
 
