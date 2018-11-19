ddfbbfe923fd360816317318154f38a13ba3aeed
Arya Pourzanjani <Arya@169-231-212-121.wireless.ucsb.edu>
2018-11-19 14:01:59 -0800

    added wieghted RMS norm

--- R/nonlinear_solve_compute_norm.R
+++ R/nonlinear_solve_compute_norm.R
@@ -3,11 +3,13 @@ compute_two_norm <- function(v) {
 }
 
 compute_rms_norm <- function(v) {
-
+  sqrt(mean(v^2))
 }
 
 create_compute_weighted_rms_norm <- function(rtols, atols) {
   function(v) {
-
+    # weights
+    w <- rtols*abs(v) + atol
+    sqrt(mean((v/w)^2))
   }
 }
