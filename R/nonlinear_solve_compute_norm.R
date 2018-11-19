5deef7681f691157d2402d5c874a5fd6ee4d24f2
Arya Pourzanjani <Arya@169-231-212-121.wireless.ucsb.edu>
2018-11-19 13:47:02 -0800

    added line search and norm computing functions

---
+++ R/nonlinear_solve_compute_norm.R
@@ -0,0 +1,13 @@
+compute_two_norm <- function(v) {
+  sqrt(sum(v^2))
+}
+
+compute_rms_norm <- function(v) {
+
+}
+
+create_compute_weighted_rms_norm <- function(rtols, atols) {
+  function(v) {
+
+  }
+}
