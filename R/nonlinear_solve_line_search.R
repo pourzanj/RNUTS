de95112275f992579a8340dc6db3f18391361579
Arya Pourzanjani <Arya@dhcp-46-204.cs.ucsb.edu>
2018-11-26 14:39:13 -0800

    fixed geometric line search

--- R/nonlinear_solve_line_search.R
+++ R/nonlinear_solve_line_search.R
@@ -4,9 +4,27 @@ create_constant_select_stepsize <- function(alpha = 1.0) {
   }
 }
 
-create_geometric_select_stepsize <- function(gamma = 2.0) {
+create_geometric_select_stepsize <- function(gamma = 0.5, MAX_ITER = 10) {
   function(iter, delta, g_x, g, Jg, Jg_v, x, compute_residual) {
-    list(alpha = gamma^iter, num_func_evals = 0, num_hess_evals = 0, num_hess_vec_prod_evals = 0)
+
+    alpha <- 1.0
+    num_func_evals <- 0
+
+    g_x_new <- g(x + alpha*delta)
+    norm_g_x <- compute_residual(g_x)
+    norm_g_x_new <- compute_residual(g_x_new)
+    num_func_evals <- num_func_evals + 1
+
+    while(norm_g_x_new > norm_g_x) {
+      alpha <- gamma*alpha
+
+      g_x_new <- g(x + alpha*delta)
+      norm_g_x_new <- compute_residual(g_x_new)
+      num_func_evals <- num_func_evals + 1
+    }
+
+
+    list(alpha = alpha, num_func_evals = num_func_evals, num_hess_evals = 0, num_hess_vec_prod_evals = 0)
   }
 }
 
